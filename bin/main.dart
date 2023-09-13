b7d0cc809e48d91ae2e0846942ed9dbd38212819
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2023-09-13 23:40:48 +0900

    Update main.dart

--- bin/main.dart
+++ bin/main.dart
@@ -1,11 +1,19 @@
 import 'package:riverpod/riverpod.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
+part 'main.g.dart';
+
 final greetProvider = Provider((ref) => 'Hello');
+
+@riverpod
+String farewell(FarewellRef ref) => 'Bye';
 
 void main() {
   final providerContainer = ProviderContainer();
 
   final hello = providerContainer.read(greetProvider);
   print(hello); // Hello
+
+  final bye = providerContainer.read(farewellProvider);
+  print(bye); // Bye
 }
