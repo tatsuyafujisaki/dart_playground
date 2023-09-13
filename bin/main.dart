4ff2653ccfd0ad6902d6f10949126cec4b98053a
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2023-09-14 01:20:14 +0900

    Update main.dart

--- bin/main.dart
+++ bin/main.dart
@@ -14,6 +14,10 @@ String myGenerated1(MyGenerated1Ref ref) => 'Hello!';
 String myGenerated2(MyGenerated2Ref ref, String firstName) =>
     'Hello, $firstName!';
 
+@riverpod
+String myGenerated3(MyGenerated3Ref ref, [String firstName1 = 'Anonymous']) =>
+    'Hello, $firstName1!';
+
 void main() async {
   final providerContainer = ProviderContainer();
 
@@ -21,15 +25,23 @@ void main() async {
       providerContainer.read(myProvider1); // The return type is String.
   print(hello); // Hello!
 
-  final helloAlice =
-      providerContainer.read(myProvider2('Jane')); // The return type is String.
-  print(helloAlice); // Hello, Alice!
+  final helloAlice = providerContainer
+      .read(myProvider2('Minnie')); // The return type is String.
+  print(helloAlice); // Hello, Minnie!
 
   final hello2 = providerContainer
       .read(myGenerated1Provider); // The return type is String.
   print(hello2); // Hello!
 
   final hello3 = providerContainer
-      .read(myGenerated2Provider('Jane')); // The return type is String.
-  print(hello3); // Hello!
+      .read(myGenerated2Provider('Mickey')); // The return type is String.
+  print(hello3); // Hello, Mickey!
+
+  final hello4 = providerContainer
+      .read(myGenerated3Provider()); // The return type is String.
+  print(hello4); // Hello, Anonymous!
+
+  final hello5 = providerContainer
+      .read(myGenerated3Provider('Donald')); // The return type is String.
+  print(hello5); // Hello, Donald!
 }
