b82e20f7292bd04ab4b572fed5f58dcbf2139ce6
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2023-09-21 22:11:12 +0900

    Update riverpod_examples.dart

--- bin/riverpod_examples.dart
+++ bin/riverpod_examples.dart
@@ -4,7 +4,7 @@ final myParameterlessProvider = Provider.autoDispose((ref) => 'Hello!');
 final mySingleParameterProvider =
     Provider.autoDispose.family<String, String>((ref, name) => 'Hello, $name!');
 
-Future<void> _examples() async {
+void showProviderExamples() {
   final providerContainer = ProviderContainer();
   print(providerContainer.read(myParameterlessProvider)); // Hello!
   print(
