019b30eca92dd30ea5ab1bc1932ec7fd57c4ba35
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2023-09-21 23:54:19 +0900

    Update riverpod_examples.dart

--- bin/riverpod_examples.dart
+++ bin/riverpod_examples.dart
@@ -5,7 +5,7 @@ final mySingleParameterProvider =
     Provider.autoDispose.family<String, String>((ref, name) => 'Hello, $name!');
 
 final myStateProvider = StateProvider.autoDispose((_) => false);
-final mySetStateProvider = StateProvider.autoDispose((_) => <int>{});
+final mySetStateProvider = StateProvider.autoDispose((_) => <String>{});
 final myMapStateProvider = StateProvider.autoDispose((_) => <String, bool>{});
 
 void showProviderExamples() {
@@ -26,12 +26,14 @@ void showStateProviderExamples() {
 void showSetStateProviderExamples() {
   final providerContainer = ProviderContainer();
   print(providerContainer.read(mySetStateProvider)); // {}
-  providerContainer.read(mySetStateProvider.notifier).update((state) => {1, 2});
-  print(providerContainer.read(mySetStateProvider)); // {1, 2}
   providerContainer
       .read(mySetStateProvider.notifier)
-      .update((state) => {...state, 3, 4});
-  print(providerContainer.read(mySetStateProvider)); // {1, 2, 3, 4}
+      .update((state) => {'a', 'b'});
+  print(providerContainer.read(mySetStateProvider)); // {'a', 'b'}
+  providerContainer
+      .read(mySetStateProvider.notifier)
+      .update((state) => {...state, 'c', 'd'});
+  print(providerContainer.read(mySetStateProvider)); // {'a', 'b', 'c', 'd'}
 }
 
 void showMapStateProviderExamples() {
@@ -39,11 +41,11 @@ void showMapStateProviderExamples() {
   print(providerContainer.read(myMapStateProvider)); // {}
   providerContainer
       .read(myMapStateProvider.notifier)
-      .update((state) => {'foo': false, 'bar': false});
-  print(providerContainer.read(myMapStateProvider)); // {foo: false, bar: false}
+      .update((state) => {'a': false, 'b': false});
+  print(providerContainer.read(myMapStateProvider)); // {a: false, b: false}
   providerContainer.read(myMapStateProvider.notifier).update((state) {
     state['bar'] = true;
     return state;
   });
-  print(providerContainer.read(myMapStateProvider)); // {foo: false, bar: true}
+  print(providerContainer.read(myMapStateProvider)); // {a: false, b: true}
 }
