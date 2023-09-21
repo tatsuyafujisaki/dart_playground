e664360e6935b2314666dee4fe59618e013cd91f
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2023-09-21 21:00:34 +0900

    Create riverpod_generator_examples.dart

--- bin/riverpod_examples.dart
+++ bin/riverpod_examples.dart
@@ -1,56 +1,13 @@
 import 'package:riverpod/riverpod.dart';
-import 'package:riverpod_annotation/riverpod_annotation.dart';
-
-part 'riverpod_examples.g.dart';
 
 final myParameterlessProvider = Provider.autoDispose((ref) => 'Hello!');
 final mySingleParameterProvider =
     Provider.autoDispose.family<String, String>((ref, name) => 'Hello, $name!');
 
-@riverpod
-String myGeneratedParameterless(MyGeneratedParameterlessRef ref) => 'Hello!';
-
-@riverpod
-String myGeneratedRequiredParameter(
-  MyGeneratedRequiredParameterRef ref,
-  String name1,
-) =>
-    'Hello, $name1!';
-
-@riverpod
-String myGeneratedOptionalParameter(
-  MyGeneratedOptionalParameterRef ref, [
-  String name1 = 'Anonymous',
-]) =>
-    'Hello, $name1!';
-
-@riverpod
-String myGeneratedRequiredParameters(
-  MyGeneratedRequiredParametersRef ref,
-  String name1,
-  int age,
-) =>
-    'Hello, $age-year-old $name1!';
-
-Future<void> examples() async {
+Future<void> _examples() async {
   final providerContainer = ProviderContainer();
-
   print(providerContainer.read(myParameterlessProvider)); // Hello!
   print(
     providerContainer.read(mySingleParameterProvider('Mickey')),
   ); // Hello, Mickey!
-  print(providerContainer.read(myGeneratedParameterlessProvider)); // Hello!
-  print(
-    providerContainer.read(myGeneratedRequiredParameterProvider('Minnie')),
-  ); // Hello, Minnie!
-  print(
-    providerContainer.read(myGeneratedOptionalParameterProvider()),
-  ); // Hello, Anonymous!
-  print(
-    providerContainer.read(myGeneratedOptionalParameterProvider('Donald')),
-  ); // Hello, Donald!
-
-  print(
-    providerContainer.read(myGeneratedRequiredParametersProvider('Goofy', 18)),
-  ); // Hello, Donald!
 }
