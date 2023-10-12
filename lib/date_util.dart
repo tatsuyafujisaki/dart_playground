7730f48670673674c1838d9a99a1ad2534882286
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2023-10-12 16:54:39 +0900

    Update date_util.dart

--- lib/date_util.dart
+++ lib/date_util.dart
@@ -6,10 +6,11 @@ Future<void> dateFormatExample() async {
     DateFormat('yyyy-MM-dd hh:mm:ss').format(DateTime.now()),
   ); // yyyy-MM-dd hh:mm:ss
 
-  await initializeDateFormatting('ja').then(
-    (_) => print(
+  await initializeDateFormatting('ja').then((_) {
+    print(DateFormat.yMMMEd('ja').format(DateTime.now())); // yyyy年MM月dd日(E)
+    print(
       DateFormat('yyyy年MM月dd日(E)', 'ja')
-          .format(DateTime.now()), // yyyy年MM月dd日(E)
-    ),
-  );
+          .format(DateTime.now()), // yyyy年MM月dd日(E) (same as above)
+    );
+  });
 }
