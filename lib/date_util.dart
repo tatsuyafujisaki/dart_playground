27758224f04ce336f7527580856117c8bca179f0
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2023-10-12 16:07:56 +0900

    Update date_util.dart

--- lib/date_util.dart
+++ lib/date_util.dart
@@ -3,10 +3,10 @@ import 'package:intl/intl.dart';
 /// Converts a [DateTime] to a [String] with a given pattern.
 ///
 /// Example:
-/// convertDateTimeToString() // today in yyyy-MM-dd
-/// convertDateTimeToString(dateTime: DateTime(2018, 7, 18)) // 2018-07-18
-/// convertDateTimeToString(newPattern: 'MM-dd-yyyy') // today in MM-dd-yyyy
-String convertDateTimeToString({
+/// convertToString() // today in yyyy-MM-dd
+/// convertToString(dateTime: DateTime(2018, 7, 18)) // 2018-07-18
+/// convertToString(newPattern: 'MM-dd-yyyy') // today in MM-dd-yyyy
+String convertToString({
   DateTime? dateTime,
   String newPattern = 'yyyy-MM-dd hh:mm:ss',
 }) =>
