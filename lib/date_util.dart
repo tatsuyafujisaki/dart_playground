f6ad22a761d290ea7c78c04d55faae8f9fe4d3da
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2023-10-12 16:48:32 +0900

    Update date_util.dart

--- lib/date_util.dart
+++ lib/date_util.dart
@@ -1,13 +1,13 @@
+import 'package:intl/date_symbol_data_local.dart';
 import 'package:intl/intl.dart';
 
-/// Converts a [DateTime] to a [String] with a given pattern.
-///
-/// Example:
-/// convertToString() // today in yyyy-MM-dd
-/// convertToString(dateTime: DateTime(2018, 7, 18)) // 2018-07-18
-/// convertToString(newPattern: 'MM-dd-yyyy') // today in MM-dd-yyyy
-String convertToString({
-  DateTime? dateTime,
-  String newPattern = 'yyyy-MM-dd hh:mm:ss',
-}) =>
-    DateFormat(newPattern).format(dateTime ?? DateTime.now());
+Future<void> dateFormatExample() async {
+  print(
+    DateFormat('yyyy-MM-dd hh:mm:ss').format(DateTime.now()),
+  ); // yyyy-MM-dd hh:mm:ss
+
+  await initializeDateFormatting('ja');
+  print(
+    DateFormat('yyyy年MM月dd日(E)', 'ja').format(DateTime.now()),
+  ); // yyyy年MM月dd日(E)
+}
