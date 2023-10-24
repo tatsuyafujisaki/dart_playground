590175dd8acbbb577e67e991d69d6bd6339e8b27
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2023-10-24 14:02:50 +0900

    Update enum_examples.dart

--- lib/examples/enum_examples.dart
+++ lib/examples/enum_examples.dart
@@ -6,4 +6,5 @@ void main() {
   print(_Fruit.values.elementAtOrNull(0)); // Apple
   print(_Fruit.values.elementAtOrNull(1)); // Orange
   print(_Fruit.values.elementAtOrNull(2)); // null
+  // print(_Fruit.values.elementAtOrNull(null)); // Compilation error
 }
