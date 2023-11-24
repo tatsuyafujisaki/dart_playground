173c6f92ec5a6db5f9e2e1ea7577450860838caf
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2023-11-24 13:54:41 +0900

    Revert "Update main.dart"
    This reverts commit ebda907ab609977e58f1f85b3997b968e1a1f411.

--- bin/main.dart
+++ bin/main.dart
@@ -1,12 +1,7 @@
 import 'dart:io';
 
 void main() {
-  final xs = <dynamic>['a', 'b', 'c'];
-  print(xs.runtimeType); // List<dynamic>
-
-  final ys = List<String>.from(xs);
-  print(ys.runtimeType); // List<String>
-
-  final zs = xs.cast<String>();
-  print(zs.runtimeType); // CastList<dynamic, String>
+  print(Platform.version);
+  print(const String.fromEnvironment('MY_VAR1'));
+  print(const String.fromEnvironment('MY_VAR2'));
 }
