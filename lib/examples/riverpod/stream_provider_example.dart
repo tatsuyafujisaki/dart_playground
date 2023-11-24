eb93c800aebe3082f7ef7ca31ff08e6d8ebb3b8f
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2023-11-24 15:55:45 +0900

    Update stream_provider_example.dart

--- lib/examples/riverpod/stream_provider_example.dart
+++ lib/examples/riverpod/stream_provider_example.dart
@@ -1,26 +1,26 @@
 import 'package:riverpod/riverpod.dart';
 
-final _streamProvider = StreamProvider.autoDispose<int>(
+final _streamProvider = StreamProvider.autoDispose<String>(
   (_) => Stream.periodic(
     Duration(seconds: 1),
-    (count) => (count < 3) ? count : throw Exception(),
+    (count) => (count < 3) ? count.toString() : throw Exception(),
   ),
 );
 
 void main() async {
   final container = ProviderContainer();
 
-  final asyncValueInt = container.read(_streamProvider) // AsyncValue<int>
+  final asyncValueInt = container.read(_streamProvider) // AsyncValue<String>
     ..when(
       data: (data) => print('data: $data'),
       error: (error, _) => print('error: $error'),
       loading: () => print('loading'),
     );
-  print(asyncValueInt); // AsyncLoading<int>()
+  print(asyncValueInt); // AsyncLoading<String>()
 
   try {
-    final future = container.read(_streamProvider.future); // Future<int>
-    print('awaited future: ${await future}'); // 0
+    final future = container.read(_streamProvider.future); // Future<String>
+    print('awaited future: ${await future}'); // '0'
   } catch (e) {
     print('awaited future error: $e');
   }
