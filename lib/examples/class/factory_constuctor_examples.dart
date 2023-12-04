a6c9ef43d603d9227fb33b2bb360098de7526d1e
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2023-12-04 14:26:16 +0900

    Update factory_constuctor_examples.dart

--- lib/examples/class/factory_constuctor_examples.dart
+++ lib/examples/class/factory_constuctor_examples.dart
@@ -1,21 +1,25 @@
 class _Person1 {
-  _Person1(this.name);
-  factory _Person1.fromJson(Map<String, String> json) => _Person1(
-        json['name']!,
+  _Person1(this.name, this.age);
+  factory _Person1.fromJson(Map<String, dynamic> map) => _Person1(
+        map['name'] as String,
+        map['age'] as int,
       );
   final String name;
+  final int age;
 }
 
 class _Person2 {
-  _Person2(Map<String, String> json) : name = json['name']!;
+  _Person2(Map<String, dynamic> map)
+      : name = map['name'] as String,
+        age = map['age'] as int;
   final String name;
+  final int age;
 }
 
 void main() {
-  final map = <String, String>{'name': 'Jane'};
+  final map = <String, dynamic>{'name': 'Jane', 'age': 18};
   final person1 = _Person1.fromJson(map);
   print(person1.name);
-
   final person2 = _Person2(map);
   print(person2.name);
 }
