cc274b932a9d96418fd59700f9785e1838c35e7f
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2023-12-05 19:27:14 +0900

    Update extensions.dart

--- lib/extensions.dart
+++ lib/extensions.dart
@@ -1,7 +1,15 @@
-extension NullNorEmpty on String? {
+import 'package:riverpod/riverpod.dart';
+
+extension on String? {
   bool get isNullOrEmpty => this?.isNotEmpty ?? true;
+  bool get isNeitherNullNorEmpty => this?.isNotEmpty ?? false;
 }
 
-extension NeitherNullNorEmpty on String? {
-  bool get isNeitherNullNorEmpty => this?.isNotEmpty ?? false;
+extension on AsyncValue<dynamic> {
+  void printAll() => when(
+        data: (data) => print('👀data=$data'),
+        error: (error, stackTrace) =>
+            print('👀error=$error, stackTrace=$stackTrace'),
+        loading: () => print('👀loading'),
+      );
 }
