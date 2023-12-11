cd8f15786bf86d122eb340a1e0be7c4e05159ac0
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2023-12-11 17:26:32 +0900

    Update singletons.dart

--- lib/examples/class/singletons.dart
+++ lib/examples/class/singletons.dart
@@ -13,15 +13,7 @@ class _Singleton2 {
   static final instance = _Singleton2._privateConstructor();
 }
 
-/// Singleton by static getter
-class Singleton3 {
-  Singleton3._privateConstructor();
-  static final _instance = Singleton3._privateConstructor();
-  static Singleton3 get instance => _instance;
-}
-
 void main() {
-  print(_Singleton1() == _Singleton1());
-  print(_Singleton2.instance == _Singleton2.instance);
-  print(Singleton3.instance == Singleton3.instance);
+  print(_Singleton1() == _Singleton1()); // true
+  print(_Singleton2.instance == _Singleton2.instance); // true
 }
