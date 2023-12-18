ae17e6b32366e5d0f46f2d26373dd703136d5541
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2023-12-18 11:28:00 +0900

    Update iterable_util.dart

--- lib/util/iterable_util.dart
+++ lib/util/iterable_util.dart
@@ -1,4 +1,16 @@
-// The functions in this file are redundant and not intended for production use.
+Iterable<T> _flatten<T>(Iterable<Iterable<T>> xs) => xs.expand((x) => x);
+Iterable<T> _filterNotNull<T>(Iterable<T?> xs) => xs.whereType<T>();
 
-Iterable<T> flatten<T>(Iterable<Iterable<T>> xs) => xs.expand((x) => x);
-Iterable<T> filterNotNull<T>(Iterable<T?> xs) => xs.whereType<T>();
+void main() {
+  final xs = [
+    ['a'],
+    ['b', 'c'],
+    ['d'],
+  ];
+
+  print(_flatten(xs).toList()); // [a, b, c, d]
+
+  final ys = <String?>['a', null, 'b'];
+
+  print(_filterNotNull(ys).toList()); // [a, b]
+}
