9c919cb06173e96b91e25cbe1f3d338f1a65ced4
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2023-12-22 10:01:33 +0900

    Update person.dart

--- lib/examples/freezed/person.dart
+++ lib/examples/freezed/person.dart
@@ -12,3 +12,11 @@ class Person with _$Person {
 
   factory Person.fromJson(Map<String, Object?> json) => _$PersonFromJson(json);
 }
+
+void main() {
+  const person = Person(name: 'John', age: 42);
+  final json = person.toJson();
+  print(json); // {name: John, age: 42}
+  final person2 = Person.fromJson(json);
+  print(person2); // Person(name: John, age: 42)
+}
