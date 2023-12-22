496aee169b84876a1f5266afd66cfc72cc53d209
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2023-12-22 12:22:15 +0900

    Update date_time_converter_test.dart

--- test/util/json/date_time_converter_test.dart
+++ test/util/json/date_time_converter_test.dart
@@ -5,27 +5,28 @@ import 'package:test/test.dart';
 
 void main() {
   test(
-    'Can convert JSON (including DateTime) to Person',
+    'Can convert String to DateTime',
     () {
       final person = Person.fromJson(
-        {
-          'name': 'John',
-          'age': 42,
-          'lastUpdated': '2024-08-26 12:34:56.123456',
-        },
+        jsonDecode(
+          '{"name":"John","age":42,"lastUpdated":"2024-01-02 12:34:56.123456"}',
+        ) as Map<String, dynamic>,
       );
       expect(person.name, 'John');
       expect(person.age, 42);
-      expect(person.lastUpdated.toString(), '2024-08-26 12:34:56.123456');
+      expect(person.lastUpdated.toString(), '2024-01-02 12:34:56.123456');
     },
   );
 
-  test('Can convert Person (including DateTime) to JSON', () {
-    final person = const Person(name: 'John', age: 42)
-        .copyWith(lastUpdated: DateTime(2024, 08, 26, 12, 34, 56, 123, 456));
-    expect(
-      jsonEncode(person),
-      '{"name":"John","age":42,"lastUpdated":"2024-08-26 12:34:56"}',
-    );
-  });
+  test(
+    'Can convert DateTime to String without subseconds',
+    () {
+      final person = const Person(name: 'John', age: 42)
+          .copyWith(lastUpdated: DateTime(2024, 01, 02, 12, 34, 56, 123, 456));
+      expect(
+        jsonEncode(person),
+        '{"name":"John","age":42,"lastUpdated":"2024-01-02 12:34:56"}',
+      );
+    },
+  );
 }
