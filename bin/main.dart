1bb616387567ef526aebf990924e82da18e87332
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2024-08-08 17:13:53 +0900

    Update main.dart

--- bin/main.dart
+++ bin/main.dart
@@ -1,7 +1,1 @@
-import 'dart:io';
-
-void main() {
-  print(Platform.version);
-  print(const String.fromEnvironment('MY_VAR1'));
-  print(const String.fromEnvironment('MY_VAR2'));
-}
+void main() {}
