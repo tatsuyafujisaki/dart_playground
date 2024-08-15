bcdd1fe9d3fd3a34cccc0c29a9c86bc7a539e6ed
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2024-08-15 19:16:22 +0900

    Delete the http package

--- lib/examples/http/http_util.dart
+++
@@ -1,9 +0,0 @@
-import 'package:http/http.dart';
-
-void printResponse(Response response) {
-  print('👀response.request: ${response.request}');
-  print('👀response.statusCode: ${response.statusCode}');
-  print('👀response.reasonPhrase: ${response.reasonPhrase}');
-  print('👀response.headers: ${response.headers}');
-  print('👀response.body: ${response.body}');
-}
