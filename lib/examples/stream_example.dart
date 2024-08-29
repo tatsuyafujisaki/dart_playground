07a266fe9327d18085680091c7bb7f4494afd15c
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2024-08-29 12:23:28 +0900

    Update stream_example.dart

--- lib/examples/stream_example.dart
+++ lib/examples/stream_example.dart
@@ -1,24 +1,32 @@
 // https://api.flutter.dev/flutter/dart-async/Stream/handleError.html
+import 'dart:async';
+
 void main() async {
-  Stream.periodic(
+  StreamSubscription<int>? subscription;
+
+  subscription = Stream.periodic(
     const Duration(milliseconds: 100),
     (count) {
       if (count == 1) {
         throw Exception('💀');
       }
+      if (count == 3) {
+        subscription?.cancel();
+      }
       return count;
     },
   )
-      .take(10)
+      .take(5)
       .handleError(
         (Object error, StackTrace stackTrace) =>
-            print('handleError: $error\n$stackTrace'),
+            print('👀handleError: $error\n$stackTrace'),
       )
       .listen(
         (data) => print('👀data: $data'),
         // onError will not be called if "Stream.handleError" is set.
         onError: (Object error, StackTrace stackTrace) =>
             print('👀onError: $error\n$stackTrace'),
+        // onDone will not be called if done by cancel.
         onDone: () => print('👀onDone'),
       );
 }
