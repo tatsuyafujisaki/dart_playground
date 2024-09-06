c0da306393fd126ecbcfe74b4c3f50b2990bdc38
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2024-09-06 11:01:51 +0900

    Update iterable_util.dart

--- lib/util/iterable_util.dart
+++ lib/util/iterable_util.dart
@@ -1,6 +1,15 @@
 Iterable<T> _flatten<T>(Iterable<Iterable<T>> xs) => xs.expand((x) => x);
 Iterable<T> _filterNotNull<T>(Iterable<T?> xs) => xs.whereType<T>();
 
+// ignore: unreachable_from_main
+List<T> deepCopyList<T>(List<T> xs) => <T>[...xs];
+
+// ignore: unreachable_from_main
+Map<K, V> deepCopyMap<K, V>(Map<K, V> xs) => {...xs};
+
+// ignore: unreachable_from_main
+Set<T> deepCopySet<T>(Set<T> xs) => <T>{...xs};
+
 void main() {
   final xs = [
     ['a'],
