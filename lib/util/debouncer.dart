f04bf3586449fb3ee2870e5a5ea558d1908dbdaf
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2024-09-06 14:40:25 +0900

    Update debouncer.dart

--- lib/util/debouncer.dart
+++ lib/util/debouncer.dart
@@ -3,32 +3,32 @@ import 'dart:async';
 import 'package:dart_playground/util/timer_util.dart';
 
 class Debouncer {
-  Debouncer(this.duration, this.callback);
+  Debouncer(this.duration);
   final Duration duration;
-  void Function() callback;
   Timer? _timer;
 
-  void run() {
+  void run(void Function() callback) {
     _timer?.cancel();
     _timer = Timer(duration, callback);
   }
 }
 
 void main() {
-  final debouncer = Debouncer(
-    const Duration(seconds: 3),
-    () => print('Debouncer: 3 seconds passed without another call.'),
-  );
+  final debouncer = Debouncer(const Duration(seconds: 3));
 
   count10();
 
   Timer(
     const Duration(seconds: 2),
-    debouncer.run,
+    () => debouncer.run(
+      () => print('Debouncer: 2 seconds passed without another call.'),
+    ),
   );
 
   Timer(
     const Duration(seconds: 4),
-    debouncer.run,
+    () => debouncer.run(
+      () => print('Debouncer: 4 seconds passed without another call.'),
+    ),
   );
 }
