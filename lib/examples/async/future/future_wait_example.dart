16380b5300deefaae13b1147aae5975e7ed5ae7d
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2024-09-17 11:09:07 +0900

    Update future_wait_example.dart

--- lib/examples/async/future/future_wait_example.dart
+++ lib/examples/async/future/future_wait_example.dart
@@ -1,37 +1,34 @@
 import 'package:dart_playground/util/timer_util.dart';
 
 void main() async {
-  final future1 = Future<String>.delayed(
-    const Duration(seconds: 5),
-    () => '🍎',
-  );
+  print('Future.wait example:');
+  await _showFutureWaitExample();
+  print('Not Future.wait example:');
+  await _showNonFutureWaitExample();
+}
 
-  final future2 = Future<String>.delayed(
-    const Duration(seconds: 5),
-    () => '🍎',
-  );
+Future<String> _createFuture() => Future<String>.delayed(
+      const Duration(seconds: 10),
+      () => '🍎',
+    );
 
+// Waits for 5 seconds in total.
+Future<void> _showFutureWaitExample() async {
   final timer = countUp();
-  await _showParallelWaitExample(future1, future2);
-  await _showSerialWaitExample(future1, future2);
+  final future1 = _createFuture();
+  final future2 = _createFuture();
+  final futures = Future.wait<String>([future1, future2]);
+  print(await futures);
   timer.cancel();
 }
 
 // Waits for 5 seconds in total.
-Future<void> _showParallelWaitExample(
-  Future<String> future1,
-  Future<String> future2,
-) async {
-  final futures = Future.wait<String>([future1, future2]);
-  print(await futures);
-}
-
-// Waits for 10 seconds in total.
-Future<void> _showSerialWaitExample(
-  Future<String> future1,
-  Future<String> future2,
-) async {
+Future<void> _showNonFutureWaitExample() async {
+  final timer = countUp();
+  final future1 = _createFuture();
+  final future2 = _createFuture();
   final result1 = await future1;
   final result2 = await future2;
   print([result1, result2]);
+  timer.cancel();
 }
