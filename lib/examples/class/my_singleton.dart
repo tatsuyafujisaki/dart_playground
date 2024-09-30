fb24b11aa8f6aba8039bb65dd6b061aa05dab695
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2024-09-30 13:21:24 +0900

    Update my_singleton.dart

--- lib/examples/class/my_singleton.dart
+++ lib/examples/class/my_singleton.dart
@@ -1,6 +1,9 @@
 /// https://stackoverflow.com/a/12649574/10867055
 class MySingleton {
-  factory MySingleton() => _singleton;
+  factory MySingleton() {
+    print('The constructor of MySingleton is called.');
+    return _singleton;
+  }
   MySingleton._privateConstructor();
   static final _singleton = MySingleton._privateConstructor();
   final initializedAt = DateTime.now();
