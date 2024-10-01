fee0d8f586502d1e163d52462bd815f5384e81b7
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2024-10-01 22:19:32 +0900

    Update my_singleton.dart

--- lib/examples/class/my_singleton.dart
+++ lib/examples/class/my_singleton.dart
@@ -1,6 +1,7 @@
 /// https://stackoverflow.com/a/12649574/10867055
 class MySingleton {
   factory MySingleton() {
+    Future.delayed(Duration.zero, () => print('Future.delayed is called.'));
     print('The constructor of MySingleton is called.');
     return _singleton;
   }
@@ -12,5 +13,5 @@ class MySingleton {
 void main() {
   print(MySingleton() == MySingleton()); // true
   print(MySingleton().initializedAt);
-  print(MySingleton().initializedAt);
+  print(MySingleton().initializedAt); // prints the same timestamp as above.
 }
