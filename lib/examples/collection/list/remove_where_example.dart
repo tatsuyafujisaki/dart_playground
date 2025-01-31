416f9dd24a85183a667acaf1764e11639e10a71d
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2025-01-31 11:41:20 +0900

    Update remove_where_example.dart

--- lib/examples/collection/list/remove_where_example.dart
+++ lib/examples/collection/list/remove_where_example.dart
@@ -1,6 +1,6 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
 
-part '../../list/remove_where_example.freezed.dart';
+part 'remove_where_example.freezed.dart';
 
 @freezed
 class Fruit with _$Fruit {
