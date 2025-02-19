8a4401424c83dacb18da1949ace00efcd1bf6349
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2025-02-19 17:43:13 +0900

    Update async_constructor_example.dart

--- lib/examples/class/async_constructor_example.dart
+++ lib/examples/class/async_constructor_example.dart
@@ -1,14 +1,15 @@
 class _MyAsyncConstructorClass {
   _MyAsyncConstructorClass._(this.data);
-  final String data;
 
-  static Future<_MyAsyncConstructorClass> create() async {
-    final data = await Future<String>.value('🍎');
+  static Future<_MyAsyncConstructorClass> create(String emoji) async {
+    final data = await Future<String>.value(emoji);
     return _MyAsyncConstructorClass._(data);
   }
+
+  final String data;
 }
 
 void main() async {
-  final myClass = await _MyAsyncConstructorClass.create();
+  final myClass = await _MyAsyncConstructorClass.create('🍎');
   print(myClass.data); // 🍎
 }
