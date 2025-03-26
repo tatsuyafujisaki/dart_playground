3d1bb18947a9e5cedbff86ff2bbc37ff3c9a248b
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2025-03-26 11:06:00 +0900

    Update future_example.dart

--- lib/examples/async/future/future_example.dart
+++ lib/examples/async/future/future_example.dart
@@ -1,10 +1,29 @@
 void main() async {
-  print(
-    await Future<String>(
-      () => '🍎',
-    ),
-  );
+  final future1 = Future<String>.value('🍎');
+  final future2 = Future<String>.error('☠️');
 
-  print(await Future.value('🍊'));
-  print(await Future.error('💀'));
+  await _printFuture(future1);
+  await _printFuture(future2);
+  await _printFuture(future1, deliberatelyThrowErrorInsideThen: true);
+}
+
+Future<void> _printFuture<T>(
+  Future<T> future, {
+  bool deliberatelyThrowErrorInsideThen = false,
+}) async {
+  await future.then(
+    (value) {
+      if (deliberatelyThrowErrorInsideThen) {
+        throw Exception('This exception originate in `then`.');
+      }
+      print('then: $value');
+    },
+    onError: (Object? error) {
+      print('then.onError: $error');
+    },
+  ).catchError(
+    (Object? error) {
+      print('catchError: $error');
+    },
+  );
 }
